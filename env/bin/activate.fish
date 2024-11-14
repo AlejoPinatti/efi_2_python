548958458f4fc700f46878b24b45d253e228383d
AlejoPinatti <a.pinatti@itecriocuarto.org.ar>
2024-11-14 20:34:11 -0300

    Tu mensaje de commit

--- env/bin/activate.fish
+++ env/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/alejo/Música/efi_2_Python/env"
+set -gx VIRTUAL_ENV "/home/alejo/Vídeos/efi_2_Python/env"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
